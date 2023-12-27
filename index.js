b9f46a2fdde4a5f25fba0fc029e315819e2ac461
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-12-27 14:18:29 +0100

    Working on deploying

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const mongoose = require('mongoose')
-const { socketSendMessage, sendNotification } = require('./controller/socket.controller');
+const { default: axios } = require('axios');
 require('dotenv').config();
 
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: '*',
-  },
+    cors: {
+        origin: '*',
+    },
 });
 
 const PORT = process.env.PORT1;
 const URI = process.env.URI
 
-let onlineUsers = [];    
+let onlineUsers = [];
 
-const addUserId =(mongoDbId, socketId)=>{
-    mongoDbId!=null&&!onlineUsers.some((user)=>user.mongoDbId == mongoDbId) && onlineUsers.push({
+const addUserId = (mongoDbId, socketId) => {
+    mongoDbId != null && !onlineUsers.some((user) => user.mongoDbId == mongoDbId) && onlineUsers.push({
         socketId,
         mongoDbId
     })
 }
 
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
     console.log('Socket accessed' + socket.id);
-    socket.on('connectSocketId', (mongoose)=>{
+    socket.on('connectSocketId', (mongoose) => {
         console.log('adding id', mongoose)
         addUserId(mongoose, socket.id)
     })
 
-    socket.on('sendMessage', (messageDetails, partnerId)=>{
-        socketSendMessage(messageDetails)
-        sendNotification({
-            senderId: messageDetails.senderId,
-            message: messageDetails.message.slice(0,15),
-            date: new Date(),
-            name: messageDetails.senderName,
-            type: 'message'
-        }, partnerId)
+    socket.on('sendMessage', (messageDetails, partnerId) => {
+        let notificationBody = {
+            message: {
+                senderId: messageDetails.senderId,
+                message: messageDetails.message.slice(0, 15),
+                date: new Date(),
+                name: messageDetails.senderName,
+                type: 'message'
+            }, partnerId
+        };
+        axios.post('https://hopeacademybackend.vercel.app/send_message', messageDetails)
+        axios.post('https://hopeacademybackend.vercel.app/send_notification', notificationBody)
         console.log(onlineUsers)
-        const user = onlineUsers.find((user) => user.mongoDbId === partnerId) 
-        if(user){
+        const user = onlineUsers.find((user) => user.mongoDbId === partnerId)
+        if (user) {
             socket.to(user.socketId).emit('getMessage', messageDetails)
             socket.to(user.socketId).emit("getNotification", {
                 senderId: messageDetails.senderId,
-                message: messageDetails.message.slice(0,15),
+                message: messageDetails.message.slice(0, 15),
                 date: new Date(),
                 name: messageDetails.senderName,
                 type: 'message'
@@ -56,48 +58,48 @@ io.on('connection', (socket)=>{
         }
     })
 
-    socket.on('userJoined', (details)=>{
+    socket.on('userJoined', (details) => {
         const roomId = details.roomId
         const createdRooms = io.sockets.adapter.rooms;
         let classGoingOn = [];
         if (createdRooms.get(roomId)) {
             classGoingOn = Array.from(createdRooms.get(roomId));
         }
-        if(classGoingOn.length==0 && details.userType=='student'){
-            socket.emit('classNotStarted', {message: 'Class not started'})
+        if (classGoingOn.length == 0 && details.userType == 'student') {
+            socket.emit('classNotStarted', { message: 'Class not started' })
             return;
         }
         socket.join(roomId);
-        socket.to(roomId).emit('userConnected', {...details, classGoingOn});
+        socket.to(roomId).emit('userConnected', { ...details, classGoingOn });
         socket.emit('sendJoinedMembers', classGoingOn)
     })
 
-    socket.on('disconnectCall', (details)=>{
+    socket.on('disconnectCall', (details) => {
         console.log(details);
         const createdRooms = io.sockets.adapter.rooms;
         let classGoingOn = [];
         // if (createdRooms.get(details.roomId)) {
-            classGoingOn = Array.from(createdRooms.get(details.roomId));
+        classGoingOn = Array.from(createdRooms.get(details.roomId));
         // }
         console.log(classGoingOn);
         socket.to(details.roomId).emit('memberDisconnected', details);
     })
 
-    socket.on('disconnect', ()=>{                                                                      
+    socket.on('disconnect', () => {
         console.log(`User with socket id ${socket.id} disconnected`);
         onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id)
     })
 })
 
 mongoose.connect(URI)
-.then((res)=>{
-    console.log('connected');
-})
-.catch((err)=>{
-    console.log(err);
-})
+    .then((res) => {
+        console.log('connected');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
